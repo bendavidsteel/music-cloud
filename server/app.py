9025122a23c51d03586217f7446b6cfc047d37fa
Ben Steel <bendavidsteel@gmail.com>
2018-11-16 13:54:28 +0000

    Downloading songs now works

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 import uuid
 import os
 import boto3
+import botocore
 from werkzeug.utils import secure_filename
 from flask import Flask, jsonify, request, send_file
 from flask_cors import CORS, cross_origin
@@ -12,22 +13,31 @@ ALLOWED_EXTENSIONS = set(['mp3'])
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 USERS_TABLE = os.environ['USERS_TABLE']
 IS_OFFLINE = os.environ.get('IS_OFFLINE')
+BUCKET_NAME = 'local-bucket'
 
 # instantiate the app
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['S3_LOCATION'] = 'http://localhost:8001/'
 
 # setting up server for online or offline
 if IS_OFFLINE:
-    client = boto3.client(
+    db = boto3.client(
         'dynamodb',
         region_name='localhost',
         endpoint_url='http://localhost:8000'
     )
+
+    s3 = boto3.client(
+        's3',
+        region_name='localhost',
+        endpoint_url='http://localhost:8001'
+    )
 else:
-    client = boto3.client('dynamodb')
+    db = boto3.client('dynamodb')
+    s3 = boto3.client('s3')
 
 # enable CORS
 cors = CORS(app, resources={r"/*": {"origins": "*"}}, send_wildcard=True)
@@ -60,7 +70,7 @@ def all_songs():
             artist = request.form.get('artist')
 
             # posting song to db
-            resp = client.put_item(
+            resp = db.put_item(
                 TableName=USERS_TABLE,
                 Item = {
                     'songId': {'S': song_id},
@@ -70,8 +80,8 @@ def all_songs():
                 }
             )
 
-            file_path = os.path.join(BASEDIR, app.config['UPLOAD_FOLDER'], file_name)
-            submitted_file.save(file_path)
+            output = upload_file_to_s3(submitted_file, file_name, BUCKET_NAME)
+            print(output)
         else:
             return jsonify({'error': 'Please provide valid file'}),400
 
@@ -79,7 +89,7 @@ def all_songs():
     
     if request.method == 'GET':
         # fetching songs from db
-        resp = client.scan(
+        resp = db.scan(
             TableName = USERS_TABLE
         )
 
@@ -103,7 +113,7 @@ def single_song(song_id):
     response_object = {'status': 'success'}
     if request.method == "GET":
 
-        resp = client.get_item(
+        resp = db.get_item(
             TableName = USERS_TABLE,
             Key = {
                 'songId': {'S':song_id }
@@ -115,26 +125,47 @@ def single_song(song_id):
             return jsonify({'error': 'Song does not exist'}), 404
 
         file_name = item.get('fileName').get('S')
-
-        file_path = os.path.join(BASEDIR, app.config['UPLOAD_FOLDER'], file_name)
         
-        try:
-            response = send_file(file_path, 
-                                    as_attachment=True,
-                                    mimetype='audio/mpeg')
-            
-            response.headers.add('Access-Control-Allow-Origin', '*')
+        # try:
+        obj = s3.get_object(
+            Bucket = BUCKET_NAME,
+            Key = file_name
+        )
 
-            return response
+        fileobj = obj['Body']
+
+        response = send_file(fileobj, 
+                                as_attachment=True,
+                                attachment_filename=file_name,
+                                mimetype='audio/mpeg')
         
-        except Exception as e:
-            print(e.with_traceback)
-            return jsonify({'error': 'Unable to deliver song file'}), 400
+        response.headers.add('Access-Control-Allow-Origin', '*')
+
+        return response
+        
+        # except Exception as e:
+        #     print(e)
+        #     return jsonify({'error': 'Unable to deliver song file'}), 400
 
         
 
 
     if request.method == 'PUT':
+
+        resp = db.get_item(
+            TableName = USERS_TABLE,
+            Key = {
+                'songId': {'S':song_id }
+            }
+        )
+        item = resp.get('Item')
+
+        if not item:
+            return jsonify({'error': 'Song does not exist'}), 404
+
+        name= item.get('name').get('S')
+        artist = item.get('artist').get('S')
+        file_name = item.get('fileName').get('S')
 
         # saving song file
         submitted_file = request.files.get('file')
@@ -147,45 +178,29 @@ def single_song(song_id):
             name = request.form.get('name')
             artist = request.form.get('artist')
 
-            print(name)
-
-            # posting song to db
-            resp = client.update_item(
-                TableName=USERS_TABLE,
-                Key = {
-                    'songId': {'S': song_id},
-                    'name': {'S': name},
-                    'artist': {'S': artist},
-                    'fileName': {'S': file_name}
-                }
-            )
-
-            submitted_file.save(os.path.join(BASEDIR, app.config['UPLOAD_FOLDER'], file_name))
+            output = upload_file_to_s3(submitted_file, file_name, BUCKET_NAME)
+            print(output)
         else:
             name = request.form.get('name')
             artist = request.form.get('artist')
 
-            print("!!!")
-            print(name)
-            print(artist)
-            print("!!!")
 
-            # posting song to db
-            resp = client.update_item(
-                TableName=USERS_TABLE,
-                Key = {
-                    'songId': {'S': song_id},
-                    'name': {'S': name},
-                    'artist': {'S': artist}
-                }
-            )
-
+        # posting song to db
+        resp = db.update_item(
+            TableName=USERS_TABLE,
+            Key = {
+                'songId': {'S': song_id},
+                'name': {'S': name},
+                'artist': {'S': artist},
+                'fileName': {'S': file_name}
+            }
+        )
 
         response_object['message'] = 'Song updated!'
 
     if request.method == 'DELETE':
 
-        resp = client.get_item(
+        resp = db.get_item(
             TableName = USERS_TABLE,
             Key = {
                 'songId': {'S': song_id }
@@ -198,7 +213,7 @@ def single_song(song_id):
 
         file_name = item.get('fileName').get('S')
 
-        resp = client.delete_item(
+        resp = db.delete_item(
             TableName=USERS_TABLE,
             Key = {
                 'songId': {'S': song_id}
@@ -208,10 +223,11 @@ def single_song(song_id):
         # if not song:
         #     return jsonify({'error': 'Unable to delete song file as it was not found'}), 404
 
-        file_path = os.path.join(BASEDIR, app.config['UPLOAD_FOLDER'], file_name)
-
         try:
-            os.remove(file_path)
+            s3.delete_object(
+                Bucket = BUCKET_NAME,
+                Key = file_name
+            )
         except Exception as e:
             print(e.with_traceback)
             return jsonify({'error': 'Unable to delete song file, please try again'}), 400
@@ -219,6 +235,27 @@ def single_song(song_id):
         response_object['message'] = 'Song removed!'
 
     return jsonify(response_object)
+
+def upload_file_to_s3(file, file_name, bucket_name, acl="public-read"):
+
+    try:
+
+        s3.upload_fileobj(
+            file,
+            bucket_name,
+            file_name,
+            ExtraArgs={
+                "ACL": acl,
+                "ContentType": file.content_type
+            }
+        )
+
+    except Exception as e:
+        print("Something Happened: ", e)
+        return e
+
+    return "{}{}/{}".format(app.config["S3_LOCATION"], BUCKET_NAME, file_name)
+
 
 
 if __name__ == '__main__':
