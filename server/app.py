87693e7356745b2bccfbdb05ee5a5bdf41d81680
Ben Steel <bendavidsteel@gmail.com>
2019-01-14 11:58:11 +0100

    App now gets recommendations from spotify

--- server/app.py
+++ server/app.py
@@ -1,7 +1,6 @@
 import uuid
 import os
 import boto3
-import botocore
 import requests
 import urllib
 import base64
@@ -14,9 +13,9 @@ DEBUG = True
 UPLOAD_FOLDER = 'media/songs'
 ALLOWED_EXTENSIONS = set(['mp3'])
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
-USERS_TABLE = os.environ['USERS_TABLE']
+TRACK_TABLE = os.environ['TRACK_TABLE']
+BUCKET = "music-cloud-dev-s3tracks-mdm9dny0wg6g"
 IS_OFFLINE = os.environ.get('IS_OFFLINE')
-BUCKET_NAME = 'local-bucket'
 SPOTIFY_CLIENT_ID = "3e0d9d7298e44ab5994a9595100aff22"
 SPOTIFY_CLIENT_SECRET = "00bb019487644908b9202a88945259ff"
 
@@ -25,27 +24,29 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['CORS_HEADERS'] = 'Content-Type'
-app.config['S3_LOCATION'] = 'http://localhost:8001/'
-
-# setting up server for online or offline
-if IS_OFFLINE:
-    db = boto3.client(
-        'dynamodb',
-        region_name='localhost',
-        endpoint_url='http://localhost:8000'
-    )
-
-    s3 = boto3.client(
-        's3',
-        region_name='localhost',
-        endpoint_url='http://localhost:8001'
-    )
-else:
-    db = boto3.client('dynamodb')
-    s3 = boto3.client('s3')
 
 # enable CORS
-cors = CORS(app, resources={r"/*": {"origins": "*"}}, send_wildcard=True)
+CORS(app)#resources={r"/*": {"origins": "*"}}, send_wildcard=True)
+
+
+# setting up server for online or offline
+# if IS_OFFLINE:
+#     db = boto3.client(
+#         'dynamodb',
+#         region_name='localhost',
+#         endpoint_url='http://localhost:8000'
+#     )
+
+#     s3 = boto3.client(
+#         's3',
+#         region_name='localhost',
+#         endpoint_url='http://localhost:8001'
+#     )
+# else:
+db = boto3.client('dynamodb',
+                  region_name='us-east-1')
+s3 = boto3.client('s3',
+                  region_name='us-east-1')
 
 # file extension checking
 def allowed_filename(filename):
@@ -62,6 +63,7 @@ def all_songs():
     response_object = {'status': 'success'}
     if request.method == 'POST':
 
+        #try:
         song_url = "unavailable"
 
         song_id = uuid.uuid4().hex
@@ -69,24 +71,42 @@ def all_songs():
         name = request.form.get('name')
         artist = request.form.get('artist')
 
-        # saving song file
-        submitted_file = request.files.get('file')
-        if submitted_file and allowed_filename(submitted_file.filename):
+        fileP = request.form.get('file')
 
-            # name_artist = request.form.get('name') + '_' + request.form.get('artist') + '.mp3'
+        if fileP == "True":
+            file_provided = True
 
-            # file_name = secure_filename(name_artist)
             file_name = name + '--' + artist + '--' + song_id + '.mp3'
 
-            output = upload_file_to_s3(submitted_file, file_name, BUCKET_NAME)
-        
-            song_url = file_name
+            presigned = upload_file_to_s3(file_name, BUCKET)
 
-            file_provided = True
+            response_object['presigned'] = presigned
+
+            song_url = file_name
             playback = True
+
         else:
             file_provided = False
             playback = False
+
+        # saving song file
+        # submitted_file = request.files.get('file')
+        # if submitted_file and allowed_filename(submitted_file.filename):
+
+        #     # name_artist = request.form.get('name') + '_' + request.form.get('artist') + '.mp3'
+
+        #     # file_name = secure_filename(name_artist)
+        #     file_name = name + '--' + artist + '--' + song_id + '.mp3'
+
+        #     output = upload_file_to_s3(submitted_file, file_name, BUCKET)
+        
+        #     song_url = file_name
+
+        #     file_provided = True
+        #     playback = True
+        # else:
+        #     file_provided = False
+        #     playback = False
 
         image_url, preview_url, track_id, artist_id, spotify, playback = identify_song(name, artist, file_provided)
 
@@ -95,7 +115,7 @@ def all_songs():
 
         # posting song to db
         resp = db.put_item(
-            TableName=USERS_TABLE,
+            TableName=TRACK_TABLE,
             Item = {
                 'songId': {'S': song_id},
                 'name': {'S': name},
@@ -111,11 +131,16 @@ def all_songs():
         )
 
         response_object['message'] = 'Song added!'
+        
+
+        # except Exception as e:
+        #     response_object['error'] = e
+        #     response_object['with_traceback'] = e.with_traceback
     
     if request.method == 'GET':
         # fetching songs from db
         resp = db.scan(
-            TableName = USERS_TABLE
+            TableName = TRACK_TABLE
         )
 
         items = resp.get('Items')
@@ -129,7 +154,7 @@ def all_songs():
             file_name = item.get('songUrl').get('S')
 
             if file_provided:
-                song_url = '%s/%s/%s' % (s3.meta.endpoint_url, BUCKET_NAME, file_name)
+                song_url = '%s/%s/%s' % (s3.meta.endpoint_url, BUCKET, file_name)
             else:
                 song_url = file_name
 
@@ -145,7 +170,11 @@ def all_songs():
             })
 
         response_object['songs'] = SONGS
-    return jsonify(response_object)
+
+    # allowing cross origin requests
+    response = jsonify(response_object)
+    # response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
 
 
 @app.route('/songs/<song_id>', methods=['GET', 'PUT', 'DELETE'])
@@ -154,7 +183,7 @@ def single_song(song_id):
     if request.method == "GET":
 
         resp = db.get_item(
-            TableName = USERS_TABLE,
+            TableName = TRACK_TABLE,
             Key = {
                 'songId': {'S':song_id }
             }
@@ -167,7 +196,7 @@ def single_song(song_id):
         # delivering file url in s3 bucket
         file_name = item.get('songUrl').get('S')
         
-        file_url = '%s/%s/%s' % (s3.meta.endpoint_url, BUCKET_NAME, file_name)
+        file_url = '%s/%s/%s' % (s3.meta.endpoint_url, BUCKET, file_name)
 
         response_object['file_url'] = file_url
 
@@ -185,7 +214,7 @@ def single_song(song_id):
     if request.method == 'PUT':
 
         resp = db.get_item(
-            TableName = USERS_TABLE,
+            TableName = TRACK_TABLE,
             Key = {
                 'songId': {'S':song_id }
             }
@@ -204,20 +233,24 @@ def single_song(song_id):
         artist = request.form.get('artist')
 
         # saving song file
-        submitted_file = request.files.get('file')
-        if submitted_file and allowed_filename(submitted_file.filename):
+        fileP = request.form.get('file')
+
+        print(fileP)
+
+        if fileP == "True":
+
+            print("new file!")
 
             if not delete_file(file_provided, file_name):
                 print("Unable to delete old file")
 
             file_provided = True
 
-            # name_artist = request.form.get('name') + '_' + request.form.get('artist') + '.mp3'
-
-            # file_name = secure_filename(name_artist)
             file_name = name + '--' + artist + '--' + song_id + '.mp3'
 
-            output = upload_file_to_s3(submitted_file, file_name, BUCKET_NAME)
+            presigned = upload_file_to_s3(file_name, BUCKET)
+
+            response_object['presigned'] = presigned
 
         image_url, preview_url, track_id, artist_id, spotify, playback = identify_song(name, artist, file_provided)
 
@@ -226,7 +259,7 @@ def single_song(song_id):
 
         # posting song to db
         resp = db.update_item(
-            TableName=USERS_TABLE,
+            TableName=TRACK_TABLE,
             Key = {
                 'songId': {'S': song_id}
             },
@@ -251,37 +284,45 @@ def single_song(song_id):
 
     if request.method == 'DELETE':
 
-        resp = db.get_item(
-            TableName = USERS_TABLE,
-            Key = {
-                'songId': {'S': song_id }
-            }
-        )
-        item = resp.get('Item')
+        try:
+            resp = db.get_item(
+                TableName = TRACK_TABLE,
+                Key = {
+                    'songId': {'S': song_id }
+                }
+            )
+            item = resp.get('Item')
 
-        if not item:
-            return jsonify({'error': 'Song does not exist'}), 404
+            if not item:
+                return jsonify({'error': 'Song does not exist'}), 404
 
-        resp = db.delete_item(
-            TableName=USERS_TABLE,
-            Key = {
-                'songId': {'S': song_id}
-            }
-        )
+            resp = db.delete_item(
+                TableName=TRACK_TABLE,
+                Key = {
+                    'songId': {'S': song_id}
+                }
+            )
 
-        # if not song:
-        #     return jsonify({'error': 'Unable to delete song file as it was not found'}), 404
+            # if not song:
+            #     return jsonify({'error': 'Unable to delete song file as it was not found'}), 404
 
-        file_provided = item.get("fileProvided")
+            file_provided = item.get("fileProvided").get('BOOL')
 
-        file_name = item.get("song_url")
+            file_name = item.get("songUrl").get('S')
 
-        if not delete_file(file_provided, file_name):
-            return jsonify({'error': 'Unable to delete song file, please try again'}), 400
+            if not delete_file(file_provided, file_name):
+                return jsonify({'error': 'Unable to delete song file, please try again'}), 400
 
-        response_object['message'] = 'Song removed!'
+            response_object['message'] = 'Song removed!'
 
-    return jsonify(response_object)
+        except Exception as e:
+            response_object['error'] = e
+            response_object['with_traceback'] = e.with_traceback
+
+    # allowing cross origin requests
+    response = jsonify(response_object)
+    # response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
 
 @app.route('/recommend/<song_id>', methods=['GET'])
 def recommend_songs(song_id):
@@ -291,7 +332,7 @@ def recommend_songs(song_id):
 
     # get spotify song id from database
     resp = db.get_item(
-            TableName = USERS_TABLE,
+            TableName = TRACK_TABLE,
             Key = {
                 'songId': {'S':song_id }
             }
@@ -346,31 +387,48 @@ def recommend_songs(song_id):
         response_object['songs'] = SONGS
 
     except Exception as e:
-        print(e)
+        response_object['error'] = e
 
-    return jsonify(response_object)
+    # allowing cross origin requests
+    response = jsonify(response_object)
+    # response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
     
 
 # upload file to s3 bucket
-def upload_file_to_s3(file, file_name, bucket_name, acl="public-read"):
+def upload_file_to_s3(file_name, bucket_name, acl="public-read"):
 
     try:
 
-        s3.upload_fileobj(
-            file,
+        # s3.upload_fileobj(
+        #     file,
+        #     bucket_name,
+        #     file_name,
+        #     ExtraArgs={
+        #         "ACL": acl,
+        #         "ContentType": file.content_type
+        #     }
+        # )
+
+        presigned = s3.generate_presigned_post(
             bucket_name,
             file_name,
-            ExtraArgs={
+            Fields = {
                 "ACL": acl,
-                "ContentType": file.content_type
-            }
+                "Content-Type": "audio/mpeg"
+            },
+            Conditions = [
+                ["starts-with", "$Content-Type", ""],
+                ["starts-with", "$ACL", ""],
+            ],
         )
 
     except Exception as e:
         print("Something Happened: ", e)
         return e
 
-    return "{}{}/{}".format(app.config["S3_LOCATION"], BUCKET_NAME, file_name)
+    # return "{}/{}/{}".format(s3.meta.endpoint_url, BUCKET, file_name)
+    return presigned
 
 # replace spaces with %20 characters
 def replace_spaces(string):
@@ -446,7 +504,7 @@ def delete_file(file_provided, file_name):
     if file_provided:
         try:
             s3.delete_object(
-                Bucket = BUCKET_NAME,
+                Bucket = BUCKET,
                 Key = file_name
             )
         except Exception as e:
@@ -454,6 +512,8 @@ def delete_file(file_provided, file_name):
             return False
         finally:
             return True
+    else:
+        return True
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
